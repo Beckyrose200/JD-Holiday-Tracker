6afac1134e149bd73abdf53c11ac90e4bf60d21a
Rebecca Ransome <beckyrose200@aol.com>
2022-11-14 10:10:25 +0000

    Adding gov uk styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', async function(req, res, next) {
+  let data = {
+    message: 'Hello World! Gov Uk styling all working',
+    layout: 'layout.njk',
+    title: 'Nunjucks example'
+  }
+
+  res.render('index.njk', data)
+})
 
 module.exports = router;
