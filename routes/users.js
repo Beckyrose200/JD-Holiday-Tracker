35c8e4d47d0adedf3a6602c2305026043bef72cf
Simon Dunn <simondunn90@gmail.com>
2022-11-17 15:50:39 +0000

    db working

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
   res.send('respond with a resource');
 });
 
