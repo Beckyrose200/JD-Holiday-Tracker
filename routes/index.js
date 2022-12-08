cac0d7c60c03d82bcf1eab167ddb26fe7687552e
sfitz42 <shaunfitz1998@gmail.com>
2022-12-08 09:51:19 +0000

    Switched from native Date() to day.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
-let express = require('express');
-let router = express.Router();
+let express = require('express')
+let router = express.Router()
+const dayjs = require('dayjs')
+const objectSupport = require("dayjs/plugin/objectSupport")
 const createNewHoliday = require('../db/create-new-holiday')
 const getAllHolidays = require('../db/get-all-holidays')
+
+dayjs.extend(objectSupport)
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
@@ -19,39 +23,38 @@ router.get('/', async function(req, res, next) {
 })
 
 router.post('/', async function(req, res, next) {
-  const startDate = new Date(
-    req.body['start-date-year'],
-    req.body['start-date-month'] - 1,
-    req.body['start-date-day']
-  );
+  const startDate = dayjs({
+    year: req.body['start-date-year'],
+    month: req.body['start-date-month'] - 1,
+    day: req.body['start-date-day']
+  })
 
-  const endDate = new Date(
-    req.body['end-date-year'],
-    req.body['end-date-month'] - 1,
-    req.body['end-date-day']
-  );
+  const endDate = dayjs({
+    year: req.body['end-date-year'],
+    month: req.body['end-date-month'] - 1,
+    day: req.body['end-date-day']
+  })
 
-  await createNewHoliday(startDate, endDate)
-  // TODO: Save holiday object into db
-  //       Pass db object GUID to confirmation page
+  await createNewHoliday(startDate.toDate(), endDate.toDate())
 
-  res.redirect(`/confirmation?startDate=${startDate.getTime()}&endDate=${endDate.getTime()}`)
+  // TODO: Pass db object id to confirmation page
+  res.redirect(`/confirmation?startDate=${startDate.unix()}&endDate=${endDate.unix()}`)
 })
 
 /* GET confirmation page. */
 router.get('/confirmation', async function(req, res, next) {
   // TODO: Load holiday object from db rather than query param
 
-  const options = { year: 'numeric', month: 'long', day: 'numeric' }
-  const startDateStr = new Date(+req.query['startDate']).toDateString("en-GB", options)
-  const endDateStr = new Date(+req.query['endDate']).toDateString("en-GB", options)
+  const dateFormat = 'ddd MMM DD YYYY'
+  const startDate = dayjs.unix(req.query['startDate'])
+  const endDate = dayjs.unix(req.query['endDate'])
 
   let data = {
     message: 'Holiday request received',
     layout: 'layout.njk',
     title: 'Holiday request received',
-    startDate: startDateStr,
-    endDate: endDateStr
+    startDate: startDate.format(dateFormat),
+    endDate: endDate.format(dateFormat)
   }
 
   res.render('confirmation.njk', data)
